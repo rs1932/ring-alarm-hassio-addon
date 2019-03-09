1aed0c3ef325086d91286bf35c822c6a5e91b976
rs1932 <rs1932@users.noreply.github.com>
2019-03-09 13:08:10 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo Ring Alarm Integration
+echo Node Version
+node -v
+echo NPM Version
+npm -v
+git --version
+
+cp /data/options.json ring-alarm-mqtt/config.json
+
+cd ring-alarm-mqtt
+npm install
+chmod +x ring-alarm-mqtt.js
+DEBUG=ring-alarm-mqtt /ring-alarm-mqtt/ring-alarm-mqtt.js
